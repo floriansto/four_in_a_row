37cde33e10ed1d9062e8aa4aa7691345f4c28ae4
Florian Stockburger <stockburger.florian@gmail.com>
2021-09-29 23:30:03 +0200

    First playable version!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::clone::Clone;
+use std::io;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 enum Field {
@@ -6,26 +7,47 @@ enum Field {
     Player2,
 }
 
-enum Rules {
+enum WinningCombinations {
     VerticalWin,
     HorizontalWin,
 }
 
+struct Rules {
+    rules: Vec<WinningCombinations>,
+    winning_condition: usize,
+}
+
 impl Rules {
+    fn new(winning_condition: usize) -> Rules {
+        Rules {
+            rules: Vec::new(),
+            winning_condition,
+        }
+    }
+
     fn evaluate(&self, board: &Board, player: &Field) -> bool {
-        match self {
-            Rules::VerticalWin => {
-                return self.evaluate_vertical(board, player);
-            }
-            Rules::HorizontalWin => {
-                return self.evaluate_horizontal(board, player);
-            }
-        };
+        for rule in &self.rules {
+            match rule {
+                WinningCombinations::VerticalWin => {
+                    match self.evaluate_vertical(board, player) {
+                        true => return true,
+                        false => (),
+                    };
+                }
+                WinningCombinations::HorizontalWin => {
+                    match self.evaluate_horizontal(board, player) {
+                        true => return true,
+                        false => (),
+                    };
+                }
+            };
+        }
+        false
     }
 
     fn evaluate_vertical(&self, board: &Board, player: &Field) -> bool {
         for col in &board.data {
-            if col.len() < board.winning_condition {
+            if col.len() < self.winning_condition {
                 continue;
             }
             let mut vert_counter = 0;
@@ -35,7 +57,7 @@ impl Rules {
                     continue;
                 }
                 vert_counter += 1;
-                if vert_counter == board.winning_condition {
+                if vert_counter == self.winning_condition {
                     return true;
                 }
             }
@@ -44,7 +66,7 @@ impl Rules {
     }
 
     fn evaluate_horizontal(&self, board: &Board, player: &Field) -> bool {
-        let max_idx = board.data.len() - board.winning_condition;
+        let max_idx = board.data.len() - self.winning_condition;
         for (idx, col) in board.data.iter().enumerate() {
             if col.len() == 0 {
                 continue;
@@ -57,7 +79,7 @@ impl Rules {
                 if row != *player {
                     continue;
                 }
-                for idx2 in idx + 1..idx + board.winning_condition {
+                for idx2 in idx + 1..idx + self.winning_condition {
                     if board.data[idx2].len() <= row_idx {
                         skip_rows = true;
                         break;
@@ -65,7 +87,7 @@ impl Rules {
                     if board.data[idx2][row_idx] != *player {
                         break;
                     }
-                    if idx2 == idx + board.winning_condition - 1 {
+                    if idx2 == idx + self.winning_condition - 1 {
                         return true;
                     }
                 }
@@ -80,33 +102,40 @@ impl Rules {
 
 struct Board {
     rows: usize,
-    winning_condition: usize,
     data: Vec<Vec<Field>>,
-    rules: Vec<Rules>,
 }
 
 impl Board {
-    fn new(cols: usize, rows: usize, winning_condition: usize) -> Board {
+    fn new(cols: usize, rows: usize) -> Board {
         Board {
             rows,
-            winning_condition,
             data: vec![Vec::new(); cols],
-            rules: Vec::new(),
         }
     }
 
-    fn register_rule(&mut self, rule: Rules) {
-        self.rules.push(rule)
-    }
-
-    fn is_player_winning(&self, player: Field) -> bool {
-        for rule in &self.rules {
-            match rule.evaluate(self, &player) {
-                true => return true,
-                false => (),
-            };
+    fn draw(&self) {
+        let s = "-".repeat(self.data.len() * 2 + 1);
+        println!("{}", s);
+        for row in (0..self.rows).rev() {
+            let mut idx = 0;
+            for col in &self.data {
+                if col.len() <= row {
+                    print! {"| "};
+                } else {
+                    if col[row] == Field::Player1 {
+                        print!("|o");
+                    } else {
+                        print!("|x");
+                    }
+                }
+                if idx == self.data.len() - 1 {
+                    print!("|");
+                }
+                idx += 1;
+            }
+            println!();
+            println!("{}", s);
         }
-        false
     }
 
     fn put_stone_in_col(&mut self, player: Field, col: usize) -> Result<Field, &str> {
@@ -122,64 +151,78 @@ impl Board {
     }
 }
 
-struct BoardVisual {
-    board: Board,
+struct Game {
+    rules: Rules,
+    players: Vec<Field>,
 }
 
-impl BoardVisual {
-    fn new(board: Board) -> BoardVisual {
-        BoardVisual { board }
+impl Game {
+    fn new(winning_condition: usize) -> Game {
+        Game {
+            rules: Rules::new(winning_condition),
+            players: Vec::new(),
+        }
     }
 
-    fn draw(&self) {
-        let s = "-".repeat(self.board.data.len() * 2 + 1);
-        println!("{}", s);
-        for row in (0..self.board.rows).rev() {
-            let mut idx = 0;
-            for col in &self.board.data {
-                if col.len() <= row {
-                    print! {"| "};
-                } else {
-                    if col[row] == Field::Player1 {
-                        print!("|o");
-                    } else {
-                        print!("|x");
+    fn register_rule(&mut self, rule: WinningCombinations) {
+        self.rules.rules.push(rule)
+    }
+
+    fn is_player_winning(&self, board: &Board, player: Field) -> bool {
+        match self.rules.evaluate(board, &player) {
+            true => return true,
+            false => return false,
+        };
+    }
+
+    fn add_player(&mut self, player: Field) {
+        self.players.push(player)
+    }
+
+    fn run(&mut self) {
+        let mut board = Board::new(7, 5);
+
+        self.register_rule(WinningCombinations::HorizontalWin);
+        self.register_rule(WinningCombinations::VerticalWin);
+
+        self.add_player(Field::Player1);
+        self.add_player(Field::Player2);
+
+        loop {
+            for p in &self.players {
+                let name;
+                match p {
+                    Field::Player1 => name = "Player 1".to_string(),
+                    Field::Player2 => name = "Player 2".to_string(),
+                }
+                board.draw();
+                println!("{} it's your turn!", name);
+                let mut player_col = String::new();
+                io::stdin()
+                    .read_line(&mut player_col)
+                    .expect("Failed to read line");
+
+                let selection: usize = player_col.trim().parse().expect("Please type a number!");
+
+                match board.put_stone_in_col(*p, selection) {
+                    Err(e) => println!("Column {}: {}", selection, e),
+                    Ok(_) => (),
+                };
+
+                match self.is_player_winning(&board, *p) {
+                    true => {
+                        println!("{} wins!", name);
+                        board.draw();
+                        return;
                     }
-                }
-                if idx == self.board.data.len() - 1 {
-                    print!("|");
-                }
-                idx += 1;
+                    false => println!("Next player!"),
+                };
             }
-            println!();
-            println!("{}", s);
         }
     }
 }
 
 fn main() {
-    let mut board = Board::new(7, 5, 4);
-
-    board.register_rule(Rules::HorizontalWin);
-    board.register_rule(Rules::VerticalWin);
-
-    match board.put_stone_in_col(Field::Player2, 2) {
-        Err(e) => println!("{}", e),
-        Ok(_) => (),
-    };
-
-    for i in 0..10 {
-        match board.put_stone_in_col(Field::Player1, i) {
-            Err(e) => println!("Column {}: {}", i, e),
-            Ok(_) => (),
-        };
-    }
-
-    match board.is_player_winning(Field::Player1) {
-        true => println!("Player 1 wins"),
-        false => println!("No win"),
-    };
-
-    let board_v = BoardVisual::new(board);
-    board_v.draw();
+    let mut game = Game::new(4);
+    game.run();
 }
